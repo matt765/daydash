b4fff20c0deaf677772655dfc21e81813de54a67
matt765 <mateusz.wyrebek@gmail.com>
2021-08-22 00:10:09 +0200

    facts api

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,9 +3,8 @@ import { useState, useEffect } from 'react';
 
 function Welcome(props) {
     const [cityValue, setCityValue] = useState(props.city)
-    const [timezone, setTimezone] = useState("")
-    const [firstName, setFirstname] = useState("")
-    const [secondName, setSecondname] = useState("")
+    const [countryCode, setCountryCode] = useState()
+    const [fact, setFact] = useState()
     const [today, setToday] = useState("")
     const [monthDay, setMonthday] = useState("")
     const [monthName, setMonthname] = useState("")
@@ -27,51 +26,37 @@ function Welcome(props) {
         setYear(year)
     }, [])
 
-    useEffect(() => {
-        let key = '7485738ba74b5d861b91ad0f7b6ae2dc';
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityValue + '&appid=' + key)
-            .then(function (resp) {
-                return resp.json()
-            })
-            .then(function (data) {
-                setTimezone(data.sys.country)
-            })
 
-    }, [cityValue]);
+
 
     useEffect(() => {
-        fetch(`https://api.abalin.net/today?country=${timezone}`)
-            .then(function (resp) {
-                return resp.json()
-            })
-            .then(function (data) {
-                if (timezone) {
-                    for (var key in data.data.namedays) {
-                        if (data.data.namedays[key] !== "n/a") {
-                            if (data.data.namedays.hasOwnProperty(key)) {
-                                let namesArray = data.data.namedays[key].split(',').slice(0, 2)
-                                setFirstname(namesArray[0])
-                                setSecondname(namesArray[1])
-                            }
-                        } else {
-                            setCityValue("Washington")
-                        }
+
+        const fetchFacts = () => {
+            fetch(`https://uselessfacts.jsph.pl/random.json?language=en`)
+                .then(function (resp) {
+                    return resp.json()
+                })
+                .then((resp) => {
+
+                    if (resp.text.length > 100) {
+                        fetchFacts()
                     }
+                    else {
+                        setFact(resp.text)
+                    }
+                })
+               
+        }
 
-                }
 
-            })
-            .catch(function () {
-                // document.getElementById("noCity").classList.toggle("invisible");
-            });
-
-    }, [timezone])
+        fetchFacts()
+    }, [])
 
     return (
         <div className="welcome-container main-component" data-aos="fade-down" data-aos-duration="1000">
             <div className="welcome-name">Hello <br /> {props.name}</div>
             <div className="welcome-date">Today is <span className="welcome-subtext">{today ? today : ""}, {monthDay ? monthDay : ""} {monthName ? monthName : ""} {year ? year : ""}</span></div>
-            <div className="welcome-names">Name day of {firstName ? firstName : ""}  {secondName ? `and ${secondName}` : ""}</div>
+            <div className="welcome-names">Did you know?<br /><div className="fact"> {fact ? fact : ""}</div> </div>
         </div>
     )
 }
